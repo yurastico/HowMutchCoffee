406e372719d79ac05e05e899d8ede49eabbf5701
yurastico <cunha.ysc@gmail.com>
2024-01-31 22:42:34 -0300

    clear button

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -45,12 +46,19 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _resetCounter(){}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+              onPressed: _resetCounter, 
+              icon: Icon(Icons.restart_alt))
+        ],
       ),
       body: Center(
         child: Column(
